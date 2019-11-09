63a73e8b026948c784827aedf84cbfd7eff71be0
Anthony Salmeron <anthonysalmerondev@gmail.com>
2019-11-09 13:52:50 -0500

    first

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs')//filesystem
+const url = require('url');
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }else if(page=="/master.css"){
+    fs.readFile('master.css', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.write(data);
+      res.end();
+    });
+  }else if(page=="/main.js"){
+    fs.readFile('main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    fs.readFile('error.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+});
+server.listen(8000);
