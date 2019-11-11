9ed9a391b612c4cb38fcea7dd577cf35735f2043
TimTran-Dev <55710300+TimTran-Dev@users.noreply.github.com>
2019-11-11 18:41:32 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,64 +1,21 @@
-const btn=document.querySelector('button')
+const btn = document.querySelector('button')
+let result = document.querySelector('h3')
+let firstArray = ['lil','Method','Biz','Shabba','Rock']
+let secondArray = ['Rizza','Mob','Cat','Pump','Dexter']
 
-let result=document.querySelector('h3')
-let arrOne=['coast','wud','siz','shalla','diamond']
-let arrTwo=['Rizza','Giraffe','Sphinx','Tical','Arizona']
+btn.addEventListener('click', ()=>{
+  let firstQuestion=document.querySelector(".firstQuestion").value
+  let thirdQuestion=document.querySelector(".thirdQuestion").value
+  let secondQuestion=document.querySelector(".secondQuestion").value
+  let fourthQuestion=document.querySelector(".fourthQuestion").value
+  let fifthQuestion=document.querySelector(".fifthQuestion").value
 
-btn.addEventListener('click',()=>{
-  let topOneQ=document.querySelector(".topOneQ").value
-  let topThreeQ=document.querySelector(".topThreeQ").value
-  let topTwoQ=document.querySelector(".topTwoQ").value
-  let topFourQ=document.querySelector(".topFourQ").value
-  let topFiveQ=document.querySelector(".topFiveQ").value
-  console.log(topOneQ)
 
-let sumOne=Number(topOneQ)+Number(topTwoQ)
-console.log(sumOne)
-let sumTwo=Number(topThreeQ)+Number(topFourQ)+Number(topFiveQ)
-let firstName=arrOne[sumOne]
-let lastName=arrTwo[sumTwo]
-result.textContent=`Your Wu-Tang Name Is ${firstName} ${lastName}`
-console.log(firstName,lastName)
+let firstResult=Number(firstQuestion)+Number(secondQuestion)
 
-// fetch(`/api?word=${input}`)
-// .then(res =>res.json())
-// .then(response=>{
-//   console.log(response)
-// document.querySelector('h3').innerHTML=response.res
+let secondResult=Number(thirdQuestion)+Number(fourthQuestion)+Number(fifthQuestion)
+let firstName=firstArray[firstResult]
+let lastName=secondArray[secondResult]
+result.textContent=`Welcome to the Wu-Tang culture ${firstName} ${lastName}!`
 
 })
-
-  // stringReveresed(input)
-  // checkPalidrome(input)
-  // console.log(input)
-// })
-
-// function stringReveresed(input){
-// let reverse=input.split('').reverse().join('')
-// result.appendChild(document.createTextNode(reverse))
-// console.log(reverse)
-//   return reverse
-// }
-//
-// function checkPalidrome(input,reverse){
-//   console.log(input)
-//
-//   if(input===stringReveresed(input)){
-//     let mainElement=document.createElement('section')
-//     let element=document.createElement('h4')
-//     document.body.appendChild(mainElement)
-//     element.appendChild(document.createTextNode('Is A Palindrome'))
-//     mainElement.appendChild(element)
-//     input.value=""
-//     console.log(mainElement)
-//
-//   }else {
-//     let mainElement=document.createElement('section')
-//     let element=document.createElement('h4')
-//     document.body.appendChild(mainElement)
-//     element.appendChild(document.createTextNode('Is not a Palindrome'))
-//     mainElement.appendChild(element)
-// console.log('no')
-//   }
-// // if(str===)
-// }
