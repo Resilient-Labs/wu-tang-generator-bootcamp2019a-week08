bcd6f616fa4b0a447e8d4ea6202c7674592626ee
resilient-coders36 <resilient-coders36@resilient-new3s-MacBook-Pro.local>
2020-03-16 00:09:51 -0400

    WuTangNameGenerator

---
+++ Wu-Tang-Generator/js/main.js
@@ -0,0 +1,31 @@
+/// collaborated with HouseCass Teammates on this assignment +
+/// Mentors: SAM L, Raymond O, Mauricio A.
+
+
+let search = document.querySelector('#generator')
+
+search.addEventListener('submit', function(e) {
+  e.preventDefault()
+  let data = new FormData(search);
+  let queryString = ""
+  let isFirst = true
+
+  for(let pair of data.entries()) {
+    //data entries function returns a list of lists
+    //use a for loop to build a querystring
+    if(isFirst){
+      queryString += "?"
+      isFirst = false
+    }else{
+      queryString += "&"
+    }
+    queryString += `${pair[0]}=${pair[1]}`;
+  }
+
+
+  fetch(`/api${queryString}`)
+    .then(response => response.json())
+    .then((data) => {
+      document.getElementById("result").textContent = data.name
+    });
+})
