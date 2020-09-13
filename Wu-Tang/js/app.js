054c3778963cc58c4cfd1faed381bf2dfff51117
kim-flores <kimflores.k@gmail.com>
2020-09-13 13:43:16 -0400

    first commit

---
+++ Wu-Tang/js/app.js
@@ -0,0 +1,47 @@
+let radios = document.querySelectorAll('.radioSelect')
+let summer = ['Big', 'Cheese', 'Tha', 'Master']
+let fall = ['Ol', 'Dirty', 'Tha', 'Prince']
+let winter = ['Lil', 'Gucci', 'Tha', 'Smoke']
+let spring = ['Ignint', 'Dragon', 'Tha', 'Power']
+let userName = document.querySelector('#userName');
+let button = document.querySelector('#submitBtn')
+let form = document.querySelector('.form');
+let display = document.querySelector('#display')
+
+// let username = userName.value
+let ansArr = []
+//empty array for user answers
+
+radios.forEach(radio => {
+
+  radio.addEventListener('click', () => {
+    ansArr.push(radio.value);
+  })
+
+})
+
+
+button.addEventListener('click', (e) =>{
+  e.preventDefault()
+  let response = {
+    season: ansArr[0],
+    answers: ansArr,
+    userName: userName.value
+  };
+
+    fetch(`/buildName`, {
+      method: 'POST',
+      body: JSON.stringify(response),
+    })
+    .then(response => response.json())
+    .then(data => display.innerHTML = data)
+
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+  console.log(ansArr);
+  console.log(userName.value)
+
+})
+
