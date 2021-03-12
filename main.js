6f0b5147479b963aab811da27641a7ab7bc920e2
tamika sterlin <sterlintamika@gmail.com>
2021-03-12 17:14:42 -0500

    update css

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-//sam helped with my code
+//sam helped during community and i passed the info onto my house
 
-document.querySelector('button').addEventListener('click', generateName)
+document.querySelector('button').addEventListener('click', generateFirstName)
+document.querySelector('button').addEventListener('click', generateLastName)
 
-let nameArr = ['hillary', 'will', 'carlton', 'uncle phil', 'ashley']
+let firstNameArr = ['Method', 'Snoop Doggy', 'Gambino', 'Uncle', 'Spooked']
+let lastNameArr = ['Man', 'Drizzle', 'Gunna', 'Murda Snacks', 'Jackson']
 
 
-function generateName() {
+function generateFirstName() {
   console.log('hi');
-
   let option = document.querySelectorAll('.option')
   let sum = 0
   for (let i = 0; i < option.length; i++) {
     sum += parseInt(option[i].value)
   }
   let answer = Math.round(sum / option.length)
-  document.querySelector('#result').innerText = nameArr[answer]
-  // if (option === '1') {
-  //   document.querySelector('#result').innerText = sum
-  //
-  // }
+  document.querySelector('#first').innerText = firstNameArr[answer]
+}
+
+function generateLastName() {
+  console.log('hi');
+  let option2 = document.querySelectorAll('.option2')
+  let sum2 = 0
+  for (let i = 0; i < option2.length; i++) {
+    sum2 += parseInt(option2[i].value)
+  }
+  let answer2 = Math.round(sum2 / option2.length)
+  document.querySelector('#last').innerText = lastNameArr[answer2]
 }
